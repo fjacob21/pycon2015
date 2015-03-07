c7f0c847396349d09837c92bc8a23086b1514b06
Frederic Jacob <frederic.jacob.78@gmail.com>
2015-03-06 20:45:47 -0500

    Add the spi proxy class for the Raspberry Pi

---
+++ elpiwear/Rpi/spi.py
@@ -0,0 +1,41 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Frederic Jacob
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+# Simple proxy class for access of the spi bus on the Raspberry Pi.
+#
+
+import spidev
+
+class spi:
+
+    def __init__(self, port, device, mode=0, max_speed_hz=500000):
+        self.spi = spidev.SpiDev()
+        self.spi.open(port, device)
+        self.spi.max_speed_hz=max_speed_hz
+        self.spi.mode = mode
+
+    def write(self, data):
+        self.spi.writebytes(data)
+
+    def read(self):
+        return self.spi.readbytes()
