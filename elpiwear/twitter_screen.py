d195db2857644c52fb4bf002e3f05fa3d1f9c8f6
Frederic Jacob <frederic.jacob.78@gmail.com>
2015-04-01 20:31:16 -0400

    Change the draw call to use the screen class

--- elpiwear/twitter_screen.py
+++ elpiwear/twitter_screen.py
@@ -6,21 +6,6 @@ import twitter
 import urllib
 import textwrap
 
-def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
-    # Get rendered font width and height.
-    draw = ImageDraw.Draw(image)
-    width, height = draw.textsize(text, font=font)
-    # Create a new image with transparent background to store the text.
-    textimage = Image.new('RGBA', (width, height), (0,0,0,0))
-    # Render the text.
-    textdraw = ImageDraw.Draw(textimage)
-    textdraw.text((0,0), text, font=font, fill=fill)
-    # Rotate the text image.
-    rotated = textimage.rotate(angle, expand=1)
-    # Paste the text into the image, using it as a mask for transparency.
-    image.paste(rotated, position, rotated)
-    return width, height
-
 class twitter_screen(screen.screen):
 
     def __init__(self):
@@ -32,7 +17,7 @@ class twitter_screen(screen.screen):
         self.token_key = "lkXmLQ3WvaOEuL7C0DhMedn3vL18xiSXNEGy3grIj267x"
         self.con_secret = "Mw1OHJoR7MEKBb4JyfW7twMbP"
         self.con_secret_key = "jsSWH9w5xF7102MTEvoYixECFzIh4ZeaPp9H30baSxW2UISl4R"
-        self.back = Image.new('RGBA', (320, 240), (0,0,0,0))
+        #self.back = Image.new('RGBA', (320, 240), (0,0,0,0))
         self.twitter = twitter.Twitter(auth=twitter.OAuth(self.token, self.token_key, self.con_secret, self.con_secret_key))
         self.twimage = Image.open(self.param["logo"])
         self.twimage = self.twimage.resize((40,40))
@@ -43,13 +28,15 @@ class twitter_screen(screen.screen):
 
     def display_last_tweet(self):
         pycon = self.get_latess_tweet()
-        self.back.putdata([self.param["backcolor"]]*(240*320))
 
-        ax,ay = self.display_avatar(pycon)
-        ux,uy = self.display_user(pycon, ax+5)
-        self.back.paste(self.twimage,(ux+30,5,ux+30+40,45),self.twimage)
-        self.display_text(pycon, 0, ay+5)
-        self.img.paste(self.back.rotate(-90),(0,0,240,320))
+        self.back.putdata([self.param["backcolor"]]*(240*320))
+        ax,ay = self.display_avatar(pycon, (5,5))
+        ux,uy = self.display_user(pycon, (ax+10, 5))
+        self.draw_image(self.twimage,(ax+10+ux+10,5),self.twimage)
+        #self.back.paste(self.twimage,(ux+30,5,ux+30+40,45),self.twimage)
+        self.display_text(pycon, (0, ay+10))
+        #self.img.paste(self.back.rotate(-90),(0,0,240,320))
+        screen.screen.update(self)
 
     def get_latess_tweet(self):
         # Get your "home" timeline
@@ -62,24 +49,16 @@ class twitter_screen(screen.screen):
         urllib.urlretrieve (avatar, avatar_filename)
         return {"user":user,"name":name,"text":text,"avatar":avatar_filename}
 
-    def display_avatar(self,pycon):
+    def display_avatar(self,pycon, position):
         self.image = Image.open(pycon["avatar"])
         self.image = self.image.resize((55,55))
-        self.back.paste(self.image,(5,5,60,60))
-        return 60,60
+        #self.back.paste(self.image,(5,5,60,60))
+        return self.draw_image(self.image, position)
 
-    def display_user(self,pycon,x=60,y=0):
-        nw,nh = draw_rotated_text(self.back, pycon["name"], (x, y), 0, ImageFont.truetype('Montserrat-Bold.ttf', 20), fill=self.param["textcolor"])
-        uw,uh = draw_rotated_text(self.back, '@' + pycon["user"], (x, y+25), 0, ImageFont.truetype('Montserrat-Regular.ttf', 20), fill=self.param["textcolor"])
-        return x+max(nw,uw),y+25+uh
+    def display_user(self,pycon, position):
+        nw,nh = self.draw_text(pycon["name"], position, ImageFont.truetype('Montserrat-Bold.ttf', 20), fill=self.param["textcolor"])
+        uw,uh = self.draw_text('@' + pycon["user"], (position[0], position[1]+nh+5), ImageFont.truetype('Montserrat-Regular.ttf', 20), fill=self.param["textcolor"])
+        return max(nw,uw),nh+uh+5
 
-    def display_text(self, pycon,x=0,y=60):
-        self.draw_text_wrap(pycon["text"], 25, ImageFont.truetype('Montserrat-Regular.ttf', 22), self.back, x, y)
-
-    def draw_text_wrap(self, text, wrap, font, img, x=0, y=60):
-        lines = textwrap.wrap(text, width = wrap)
-        y_text = y
-        for line in lines:
-            width, height = font.getsize(line)
-            draw_rotated_text(img, line, (x, y_text), 0,font, fill=self.param["textcolor"])
-            y_text += height
+    def display_text(self, pycon,position):
+        self.draw_text_wrap(pycon["text"], position, 25, ImageFont.truetype('Montserrat-Regular.ttf', 22), fill=self.param["textcolor"])
