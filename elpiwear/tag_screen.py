ed68230e7f24bd0cb12067ca852e633b02a2c0ef
Frederic Jacob <frederic.jacob.78@gmail.com>
2015-04-07 22:36:21 -0400

    Add the time to the tag screen and make the name bigger

--- elpiwear/tag_screen.py
+++ elpiwear/tag_screen.py
@@ -2,6 +2,7 @@ import Image
 import ImageDraw
 import ImageFont
 import screen
+import time
 
 class tag_screen(screen.screen):
 
@@ -15,8 +16,9 @@ class tag_screen(screen.screen):
     def display_tag(self):
         self.back.putdata([(255,255,255)]*(240*320))
         lw,lh = self.draw_image(self.me, (0,0))
-        self.draw_text('Frederic Jacob', (lw + 3, 0), ImageFont.truetype('Montserrat-Bold.ttf', 25), fill=(0,0,0))
-        self.draw_text('Software Engineer', (lw + 3, 30), ImageFont.truetype('Montserrat-Regular.ttf', 20), fill=(0,0,0))
+        self.draw_text('Frederic Jacob', (lw + 3, 0), ImageFont.truetype('Montserrat-Bold.ttf', 28), fill=(0,0,0))
+        self.draw_text('Software Engineer', (lw + 3, 30), ImageFont.truetype('Montserrat-Regular.ttf', 23), fill=(0,0,0))
+        self.draw_text(time.strftime('%H:%M'), (lw + 70, 70), ImageFont.truetype('Montserrat-Regular.ttf', 23), fill=(0,0,0))
         self.draw_text('C/C++, Python...', (5, lh+10), ImageFont.truetype('Montserrat-Regular.ttf', 25), fill=(0,0,0))
         self.draw_text('Linux kernel contributor', (5, lh + 40), ImageFont.truetype('Montserrat-Regular.ttf', 25), fill=(0,0,0))
         twimg = Image.open("Twitter_logo_blue.png").resize((20,20))
@@ -27,4 +29,5 @@ class tag_screen(screen.screen):
         self.draw_text(': fjacob21', (gw + 10, lh + 100), ImageFont.truetype('Montserrat-Regular.ttf', 25), fill=(0,0,0))
 
     def update(self):
+        self.display_tag()
         return screen.screen.update(self)
